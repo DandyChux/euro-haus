b2f9657024d7b1fc3af8c744121c99d1ba980d36
Chuck Okoroji <dandychux@gmail.com>
2025-08-24 15:23:25 -0400

    refactor: added social links to footer

--- src/components/social-section.tsx
+++ src/components/social-section.tsx
@@ -11,7 +11,7 @@ export function SocialSection() {
 					</div>
 					<div className="flex space-x-4">
 						<a
-							href="https://instagram.com"
+							href="https://instagram.com/theeurohaus"
 							className="rounded-full bg-muted p-3 text-muted-foreground hover:bg-primary hover:text-primary-foreground"
 						>
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinejoin="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-brand-instagram"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 8a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" /><path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /><path d="M16.5 7.5v.01" /></svg>
@@ -25,7 +25,7 @@ export function SocialSection() {
 							<span className="sr-only">Facebook</span>
 						</a>
 						<a
-							href="https://youtube.com"
+							href="https://youtube.com/@TheEuroHaus"
 							className="rounded-full bg-muted p-3 text-muted-foreground hover:bg-primary hover:text-primary-foreground"
 						>
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinejoin="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-brand-youtube"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M2 8a4 4 0 0 1 4 -4h12a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-12a4 4 0 0 1 -4 -4v-8z" /><path d="M10 9l5 3l-5 3z" /></svg>
